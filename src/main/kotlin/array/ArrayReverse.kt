707246338b9c1387224bbf8e8c4fdc71774cc2c6
alidehkhodaei <alidehkhodaei1998@gmail.com>
2023-05-11 20:08:39 +0330

    Refactor ArrayReverse and ArrayReverseTest class

--- src/main/kotlin/array/ArrayReverse.kt
+++ src/main/kotlin/array/ArrayReverse.kt
@@ -3,12 +3,16 @@ package array
 /**
  * The function then reverses the elements of the array between the starting and ending indices using a while loop
  * and a temporary variable `temp`. Finally, the function returns the reversed array.
- * @param array: A array
- * @param start: start index array
- * @param end: end index array
- * @return Reverses elements in the array.
- */
+ * @param array a array
+ * @param start start index array
+ * @param end end index array
+ * @throws IllegalArgumentException if the [start] index is greater than the [end] index
+ * @return reverses elements in the array.
+ **/
 fun <T> reverseArray(array: Array<T>, start: Int, end: Int): Array<T> {
+    val minimumSizeArrayForReversal = 2
+    if (start == end || array.size < minimumSizeArrayForReversal) return array
+    if (start > end) throw IllegalArgumentException("The start should not be bigger than the end!")
     var index = start
     var lastIndex = end
     while (index < lastIndex) {
--- src/test/kotlin/array/ArrayReverseTest.kt
+++ src/test/kotlin/array/ArrayReverseTest.kt
@@ -2,6 +2,7 @@ package array
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 /**
  *  The ArrayReverseTest class contains test cases for the reverseArray function that reverses elements in an array.
@@ -9,24 +10,70 @@ import org.junit.jupiter.api.Test
 class ArrayReverseTest {
 
     @Test
-    fun testReverseArray() {
-        // Test case 1
-        val arr1 = arrayOf(1, 2, 3, 4, 5)
-        val result1 = reverseArray(arr1, 0, arr1.size - 1)
-        val expected1 = arrayOf(5, 4, 3, 2, 1)
-        assertArrayEquals(expected1, result1)
+    fun `reverseArray should correctly reverse the entire array`() {
+        val arr = arrayOf(1, 2, 3, 4, 5)
+        val result = reverseArray(arr, 0, arr.size - 1)
+        val expected = arrayOf(5, 4, 3, 2, 1)
+        assertArrayEquals(expected, result)
+    }
 
-        // Test case 2
-        val arr2 = arrayOf("Ali", "Shabnam", "Reza", "Fatemeh")
-        val result2 = reverseArray(arr2, 1, 2)
-        val expected2 = arrayOf("Ali", "Reza", "Shabnam", "Fatemeh")
-        assertArrayEquals(expected2, result2)
+    @Test
+    fun `reverseArray should correctly reverse a subset of the array`() {
+        val arr = arrayOf("Ali", "Shabnam", "Reza", "Fatemeh")
+        val result = reverseArray(arr, 1, 2)
+        val expected = arrayOf("Ali", "Reza", "Shabnam", "Fatemeh")
+        assertArrayEquals(expected, result)
+    }
 
-        // Test case 3
-        val arr3 = arrayOf(5.5, 4.4, 3.3, 2.2, 1.1)
-        val result3 = reverseArray(arr3, 2, 4)
-        val expected3 = arrayOf(5.5, 4.4, 1.1, 2.2, 3.3)
-        assertArrayEquals(expected3, result3)
+    @Test
+    fun `reverseArray should correctly reverse a subset of the array with decimal values`() {
+        val arr = arrayOf(5.5, 4.4, 3.3, 2.2, 1.1)
+        val result = reverseArray(arr, 2, 4)
+        val expected = arrayOf(5.5, 4.4, 1.1, 2.2, 3.3)
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `reverseArray should correctly handle a single element array`() {
+        val arr = arrayOf(1)
+        val result = reverseArray(arr, 0, arr.size - 1)
+        val expected = arrayOf(1)
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `reverseArray should correctly handle an empty array`() {
+        val arr = emptyArray<Int>()
+        val result = reverseArray(arr, 0, arr.size - 1)
+        val expected = emptyArray<Int>()
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `reverseArray when start and end are equal`() {
+        val array = arrayOf(1, 2, 3, 4, 5)
+        val start = 2
+        val end = 2
+        val reversedArray = reverseArray(array, start, end)
+        assertArrayEquals(array, reversedArray)
+    }
+
+    @Test
+    fun `reverseArray should throw ArrayIndexOutOfBoundsException if startIndex is less than 0`() {
+        val arr = arrayOf(1, 2, 3, 4, 5)
+        assertThrows<ArrayIndexOutOfBoundsException> { reverseArray(arr, -1, 3) }
+    }
+
+    @Test
+    fun `reverseArray should throw ArrayIndexOutOfBoundsException if endIndex is greater than the array size minus 1`() {
+        val arr = arrayOf(1, 2, 3, 4, 5)
+        assertThrows<ArrayIndexOutOfBoundsException> { reverseArray(arr, 2, 5) }
+    }
+
+    @Test
+    fun `reverseArray should throw ArrayIndexOutOfBoundsException if startIndex is greater than endIndex`() {
+        val arr = arrayOf(1, 2, 3, 4, 5)
+        assertThrows<IllegalArgumentException> { reverseArray(arr, 3, 2) }
     }
 
 }
