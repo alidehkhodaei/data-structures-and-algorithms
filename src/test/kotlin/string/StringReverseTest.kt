bee96bb1593a78ee1f0efaa10bbd7de7545365b1
alidehkhodaei <alidehkhodaei1998@gmail.com>
2023-08-23 13:22:24 +0330

    Refactor StringReverseTest class

--- src/test/kotlin/string/StringReverseTest.kt
+++ src/test/kotlin/string/StringReverseTest.kt
@@ -1,32 +1,24 @@
 package string
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
 
 /**
- * The StringReverseTest class contains test cases for the reverseString function, which tests the ability to reverse a
- * given string.
+ * The StringReverseTest class contains test cases for the reverseString
+ * function, which tests the ability to reverse a given string.
  */
 class StringReverseTest {
 
-    @Test
-    fun `testReverseString should return the reversed string for valid input`() {
-        val expected = "Shabnam"
-        val actual = reverseString("manbahS")
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `testReverseString should return the same string for valid input with a single character`() {
-        val expected = "a"
-        val actual = reverseString("a")
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `testReverseString should return an empty string for empty input`() {
-        val expected = ""
-        val actual = reverseString("")
+    @ParameterizedTest
+    @CsvSource(
+        "Kotlin, niltoK",
+        "a, a",
+        "Kotlin is awesome, emosewa si niltoK",
+        "'', ''"
+    )
+    fun `test reverseString function`(input: String, expected: String) {
+        val actual = reverseString(input)
         assertEquals(expected, actual)
     }
 
